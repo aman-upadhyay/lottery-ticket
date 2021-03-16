b9a550ffec9a079a77143ef2b5236079bd5a6b75
aman-upadhyay <aman.upadhyay@niser.ac.in>
2021-03-16 11:46:10 +0530

    debugging conv 2d

--- test.py
+++ test.py
@@ -1,4 +1,6 @@
-import tensorflow as tf
+import numpy as np
 
-t = tf.constant([[[1, 1, 1], [2, 2, 2]], [[3, 3, 3], [4, 4, 4]]])
-print(tf.shape(t))
+p = np.load("mnist_conv2D/mnist_conv2D_data/trial1/2/same_init/final/layer3.npy")
+m = np.load("mnist_conv2D/mnist_conv2D_data/trial1/2/same_init/masks/layer3.npy")
+print(m)
+print(p)
